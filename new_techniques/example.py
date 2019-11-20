1634c59e13dca95696814011734cafa6478a6b49
Avinash Malik <avinash.malik@auckland.ac.nz>
2019-11-20 19:28:03 +1300

    Wrote a complete example.
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- new_techniques/example.py
+++ new_techniques/example.py
@@ -2,13 +2,13 @@
 from math import factorial, ceil
 from scipy.optimize import minimize
 import sympy as S
-from sympy.abc import t, theta
-import inspect
+from sympy.abc import t
+# import inspect
 
 FLT_MAX = 3.402823466e+38
 
 
-def getLipschitz(fun, x0=[0], bounds=None):
+def getLipschitz(fun, x0=[0], bounds=[(-FLT_MAX, FLT_MAX)]):
     """args:
     fun: The function whose lipschitz constant is needed
     x0: The start point near the max lipschitz constant
@@ -75,43 +75,62 @@ def getN(epsilon, C, n=0):
 
 
 # This is all done at compile time.
-if __name__ == '__main__':
+def solve():
     """Solving cos(x) <= -0.99, dx/dt=1, x(0) = 0
-    """
-    # The guard function that needs the lipschitz constant
-    def guard():
-        diff = -((S.cos(theta)+0.99).diff(theta))
-        ldiff = S.lambdify(theta, diff, 'scipy')
-        return ldiff
+    # Basic steps:
+    # 1. First compute the n terms for each ode
+    # 2. Next replace the guard with ode(t), so that it is only in t
+    # 3. Then compute the number of terms needed for g(t)
+    # 4. Finally, compute g(t) = 0 and g(t)-2g(0) = 0
 
-    # XXX: This is just a test for multivariate guards and odes
+    # 5. Note that computing number of terms "n" in taylor essentially
+    # guarantees that tᵣ - t ≤ floating point error only, specified by the
+    # polynomial solver.
+    """
+
+    # XXX: This is the theta
     def test_multivariate():
         X = S.abc.X
-        Y = S.abc.Y
-        Xdiff = -(X**2 + Y**3 + 1)
+        # Y = S.abc.Y
+        # Xdiff = -(X**2 + Y**3 + 1)
+        Xdiff = -1              # Negative because -minimize ≡ maximize
         # Xdiff = -xt.diff(X)       # The partial derivative in X
         # Now to maximize them in each variable
-        Xdiffl = S.lambdify([X, Y], Xdiff)
-        print(inspect.getsource(Xdiffl))
+        # Xdiffl = S.lambdify([X, Y], Xdiff)
+        Xdiffl = S.lambdify([X], Xdiff)
+        # print(inspect.getsource(Xdiffl))
         return Xdiffl
 
     # XXX: Just a test
     tomaximize = test_multivariate()
-    res = getLipschitz(tomaximize, [10, 10], bounds=[(-FLT_MAX, FLT_MAX),
-                                                     (-FLT_MAX, FLT_MAX)])
-    print('max value:', res)
-    print('required terms:', getN(epsilon=1e-12, C=res))
+    res = getLipschitz(tomaximize, [0])
+    print('Lipschitz value for θ:', res)
+    nx = getN(epsilon=1e-12, C=res)
+    print('required terms for θ satisfying Lipschitz constant:', nx)
+    # Now make the taylor polynomial
+    taylorxcoeffs = [5*S.pi/2, 1] + [0]*(nx-2)
+    # These are the smooth tokens
+    taylorxpoly = sum([t**i*v for i, v in zip(range(nx), taylorxcoeffs)])
+    # The theta' taylor polynomial
+    print('θ(t) = ', taylorxpoly)
+
+    # The guard function that needs the lipschitz constant
+    def guard():
+        diff = -((S.cos(taylorxpoly)+0.99).diff(t))
+        ldiff = S.lambdify(t, diff, 'scipy')
+        return ldiff
 
     # n is the number of taylor terms we need, can be computed at
     # compile time.
     C = getLipschitz(guard(), bounds=[(-FLT_MAX, FLT_MAX)])
     n = getN(epsilon=1e-12, C=C)
-    print('Lipschitz constant for cos(θ)+0.99:', C)
+    print('\nLipschitz constant for cos(%s)+0.99: %d' % (taylorxpoly, C))
     print('Number of terms in taylor needed: ', n)
 
     # Some of these parts will happen at runtime
     # Now we do the example of the ode with taylor polynomial
     cosseries1 = S.fps(S.cos((5*S.pi)/2 + t)+0.99, x0=0).polynomial(n=n)
+    print('Guard taylor polynomial:', cosseries1, '\n')
     # print(S.simplify(cosseries1))
     root = None
     try:
@@ -131,3 +150,7 @@ if __name__ == '__main__':
         print('No root for g(t)-2*g(0) = 0')
 
     print('guard Δt:', root)
+
+
+if __name__ == '__main__':
+    solve()
