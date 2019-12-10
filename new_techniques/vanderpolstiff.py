8ac245877e83e4c5a2b441a0d7b2c5cc7bfba9f5
Avinash Malik <avinash.malik@auckland.ac.nz>
2019-12-11 11:43:32 +1300

    Added a stiff example.
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- new_techniques/vanderpolstiff.py
+++ new_techniques/vanderpolstiff.py
@@ -6,7 +6,7 @@ from src.solver import Solver
 
 
 step = 0                        # The number of integration steps
-STOP_TIME = 1.5                   # in seconds
+STOP_TIME = 500                 # in seconds
 data = dict()                   # The final result
 
 
@@ -17,23 +17,32 @@ def lorenz(env, solver, cstate=0):
     # TODO: Need to fix so that this works.
     # odes = {x.diff(solver.t): (x+5)*solver.t}
 
+    # https://lucris.lub.lu.se/ws/files/34213051/JCM_2017.pdf
+    u = 500
+
     x = S.sympify('x(t)')     # x
+    y = S.sympify('y(t)')     # y
 
     # Initial values
-    vals_at_tn = {x: 2}
+    vals_at_tn = {x: 2, y: 0}
+
     global data
     data['x'] = [vals_at_tn[x]]
+    data['y'] = [vals_at_tn[y]]
     data['t'] = [0]
 
     # Ode expressions
-    xdt = -6*x + 6
+    xdt = y
+    ydt = (u*(1 - x**2)*y) - x
 
-    def location1(x, vals_at_tn):
-        # The odes for all continuous variables in location1
-        odes = {x.diff(solver.t): xdt}
+    # The odes for all continuous variables in location1
+    odes = {x.diff(solver.t): xdt, y.diff(solver.t): ydt}
 
-        # Get the tokens for x
-        dict_tokens = {x: solver.build_tokens(x, odes)}
+    # Get the tokens for x
+    dict_tokens = {x: solver.build_tokens(x, odes),
+                   y: solver.build_tokens(y, odes)}
+
+    def location1(x, y, vals_at_tn):
 
         # First get the polynomial expression from tokens
         xps = {x: solver.get_polynomial(x, tokens, vals_at_tn)
@@ -47,8 +56,11 @@ def lorenz(env, solver, cstate=0):
         # Now compute the new values for continuous variables
         vals_at_tn = {k: solver.get_vals_at_tn_h(x, vals_at_tn, h, env.now)
                       for k, x in xps.items()}
+
         data['x'].append(vals_at_tn[x])
+        data['y'].append(vals_at_tn[y])
         data['t'].append(env.now)
+
         return 0, h, vals_at_tn
 
     # The dictionary for the switch statement.
@@ -57,15 +69,15 @@ def lorenz(env, solver, cstate=0):
     }
 
     # The initial values at time 0
-    print('%f: %s' % (env.now, vals_at_tn[x]))
+    print('%f: %s %s' % (env.now, vals_at_tn[x], vals_at_tn[y]))
 
     # Now start running the system until all events are done or
     # simulation time is over.
     while(True):
-        (cstate, delta, vals_at_tn) = switch_case[cstate](x, vals_at_tn)
+        (cstate, delta, vals_at_tn) = switch_case[cstate](x, y, vals_at_tn)
         # The new values of the continuous variables
         if delta != 0:
-            print('%f: %s' % (env.now, vals_at_tn[x]))
+            print('%f: %s %s' % (env.now, vals_at_tn[x], vals_at_tn[y]))
         # This should always be the final statement in this function
         global step
         step += 1
@@ -74,7 +86,7 @@ def lorenz(env, solver, cstate=0):
 
 def main():
     # Initiaise the solver
-    solver = Solver(n=5, epsilon=1e-6)
+    solver = Solver(n=7, epsilon=1e-7)
 
     env = simpy.Environment()
     env.process(lorenz(env, solver))
