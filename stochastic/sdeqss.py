34e540171505b1f2883be5dbe1d016692f8b0b7f
Avinash Malik <avinash.malik@auckland.ac.nz>
2020-02-04 13:43:46 +1300

    Wrote the first version of the solver, but many challenges remain.
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- stochastic/sdeqss.py
+++ stochastic/sdeqss.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
-from numpy.polynomial import Polynomial as P
 import numpy as N
+import mpmath as M
 
+
+# TODO: Check for condition that you always get at least one real
+# positive root.
 
 # fxt and gxt are slope functions of x(t), t
 def compute_step(fxt, gxt, dq, R):
     # First get the number of random variables
-    dWt = N.random.standard_normal(R)
+    dWt = N.random.randn(R)
+    print('dWt:', dWt)
     Winc = sum(dWt)
     gn = gxt * Winc
     # This is the max dq we can take
@@ -22,21 +26,27 @@ def compute_step(fxt, gxt, dq, R):
     # Third coefficient
     c = R*(dq**2)
 
-    # The first polynomial ax² + bx + c = 0
-    # Build the polynomial to solve for dt
-    p = P([a, b, c])
-    roots = p.roots()
-    roots = [i for i in roots if N.isreal(i)]
-    root1 = min(roots) if len(roots) != 0 else None
-    print(p, root1)
+    # Use mpmath to get the roots
+    f = (lambda x: a*(x**2) + (b*x) + c)
+    try:
+        root1 = M.findroot(f, 0, tol=1e-3)
+        # Debug
+        print('root1:', root1)
+        root1 = root1 if root1 > 0 else None
+    except ValueError as e:
+        print(e)
+        root1 = None
 
     # The second polynomial ax² - bx + cx = 0
     b = ((2 * fxt * dq * R) + (gn**2))
-    p = P([a, -b, c])
-    roots = p.roots()
-    roots = [i for i in roots if N.isreal(i)]
-    root2 = min(roots) if len(roots) != 0 else None
-    print(p, root2)
+    f = (lambda x: a*(x**2) + (b*x) + c)
+    try:
+        root2 = M.findroot(f, 0, tol=1e-3)
+        print('root2:', root2)
+        root2 = root2 if root2 > 0 else None
+    except ValueError as e:
+        print(e)
+        root2 = None
 
     # Now get Δt and δt
     Dt = root1 if root1 is not None else root2
@@ -44,8 +54,8 @@ def compute_step(fxt, gxt, dq, R):
     dt = Dt/R
     print('Δt: %f, δt: %f' % (Dt, dt))
 
-    assert False
-    return (Dt, dt)
+    # assert False
+    return (Dt, dt, dWt)
 
 
 def main(delta=1e-10):
@@ -56,26 +66,43 @@ def main(delta=1e-10):
     # dx = λx(t)dt + μx(t)dW(t) scalar SDE example
     # λ = 2, μ = 1, x(0) = 1
 
-    fxt = (lambda x: (lambda t: 2 * x))
-    gxt = (lambda x: (lambda t: 1 * x))
+    fxt = (lambda x: (lambda t: 2 * 1))
+    gxt = (lambda x: (lambda t: 1 * 1))
     # Variables, and initial values
     x = 1                       # Initial value
-    xf = 1.1
+    xf = 2
     t = 0
     vs = [x]
     ts = [t]
+    # count = 0                   # Debug
     while(True):
-        dt = compute_step(fxt(x)(t), gxt(x)(t), (xf-x), 4)
-        t = t + dt
+        curr_fxt = fxt(x)(t)
+        curr_gxt = gxt(x)(t)
+        Dt, dt, dWt = compute_step(curr_fxt, curr_gxt, abs(xf-x), R=4)
         # Now compute x(t) using Euler-Maruyama solution to get x(t)
+        # First build the weiner process
+        dWt = N.sqrt(dt) * dWt
+        print('New dWt:', dWt)
+        Winc = sum(dWt)
+        # EM
+        x = x + (Dt * curr_fxt) + (curr_gxt * Winc)
+        # Increment the time-step
+        t = t + Dt
+        # Append to plot later on
         vs.append(x)
         ts.append(t)
-        # Check if δ <= |x(t) - xf| <= δ
+        # This does not guarantee first time
+        # XXX: Check
         if abs(x - xf) <= delta:
             break
+        # elif count > 100:
+        #     break
+        # count += 1
     return vs, ts
 
 
 if __name__ == '__main__':
+    # N.random.seed(0)
     xs, ts = main()
     plt.plot(ts, xs)
+    plt.show()
