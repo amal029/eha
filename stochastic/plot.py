23212843128a12bdea027e402ed1312619d33708
Avinash Malik <avinash.malik@auckland.ac.nz>
2020-02-15 23:28:46 +1300

    Updated plot

--- stochastic/plot.py
+++ stochastic/plot.py
@@ -2,18 +2,23 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import linregress
 
 
 def main(file=None):
     toplot = np.loadtxt(file, delimiter=',', skiprows=1)
-    xs = np.log(toplot[:, 0])
-    plt.plot(xs, np.log(toplot[:, 1]), marker='*')
+    xs = np.log(np.sort(toplot[:, 0]))
+    ys = np.log(toplot[:, 1])
+    plt.plot(xs, ys, marker='*')
     # idealy = np.sqrt(toplot[:, 0])
-    idealy = 0.5 * np.log(toplot[:, 0])
-    plt.plot(xs, idealy)
-    plt.show()
+    # idealy = 0.5 * np.log(toplot[:, 0])
+    # plt.plot(xs, idealy, marker='1')
+    # plt.title(file)
+    # plt.show()
+    print(file, ':', linregress(xs, ys)[0])
 
 
 if __name__ == '__main__':
-    main('/tmp/results/runexample4inward_5.csv')
-    main('/tmp/results/runexample4inward_10.csv')
+    import os
+    for filename in os.listdir('/tmp/results/'):
+        main('/tmp/results/'+filename)
