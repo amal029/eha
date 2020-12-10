77ba6cbd5dfd93b5cea217c40b1bfcf63ba510ee
Avinash Malik <avinash.malik@auckland.ac.nz>
2020-12-10 13:08:57 +1300

    Added the first working example of model predictive control (MPC) using SMT solver and L1 norm. It even has non-linearity in the plant model. Also, disturbances are included.
    This work should be extended to MPC for hybrid systems.
    
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- mpc/mpc.py
+++ mpc/mpc.py
@@ -1,53 +1,54 @@
 #!/usr/bin/env python3
 
 from z3 import Real, Solver, sat, If
+import numpy
 
 
-def minimize(s, obj, objv):
-    # XXX: Get a lower bound
-    s.push()
-    s.add(obj <= objv/2)
-    ret = s.check()
-    s.pop()
-    if ret == sat:
-        return minimize(s, obj, objv/2)
-    else:
-        # XXX: What to do here?
-        return objv/2, objv
+def MPC(N, M, Q, fs, refx, refu, IS, w, la, consxl, consxr, consul, consur,
+        norm=1, DEBUG=False):
 
-
-# FIXME: Make this interative
-def binary_search(lb, ub, s, obj, epsilon=1e-6):
-    if (ub - lb <= epsilon):
-        s.check()
-        return ub, s.model()
-    else:
-        half = lb + ((ub - lb)/2.0)
+    def minimize(s, obj, objv, epsilon=1e-6):
+        # XXX: Get a lower bound
         s.push()
-        s.add(obj >= lb, obj <= half)
+        s.add(obj <= objv/2)
         ret = s.check()
+        if ret == sat:
+            ro = (float(s.model()[obj].numerator_as_long())
+                  / float(s.model()[obj].denominator_as_long()))
         s.pop()
+        if ret == sat and ro <= epsilon:
+            return objv/2, objv
+        if ret == sat:
+            return minimize(s, obj, objv/2)
+        else:
+            return objv/2, objv
+
+    def binary_search(lb, ub, s, obj, epsilon=1e-6):
+        if (ub - lb <= epsilon):
+            s.check()
+            return ub, s.model()
+        else:
+            half = lb + ((ub - lb)/2.0)
+            s.push()
+            s.add(obj >= lb, obj <= half)
+            ret = s.check()
+            s.pop()
         if ret == sat:
             return binary_search(lb, half, s, obj, epsilon)
         else:
             return binary_search(half, ub, s, obj, epsilon)
 
+    def mreduce(sV, Ej):
+        def max(x, y):
+            return If(x > y, x, y)
 
-def mreduce(sV, Ej):
-    def max(x, y):
-        return If(x > y, x, y)
+        if len(Ej) == 0:
+            return sV
+        else:
+            return max(Ej[0], mreduce(sV, Ej[1:]))
 
-    if len(Ej) == 0:
-        return sV
-    else:
-        return max(Ej[0], mreduce(sV, Ej[1:]))
-
-
-def mpc(N, fs, refx, refu, IS, IC, w, la, consxl, consxr, consul, consur,
-        norm=2):
-
-    M = len(IS)
-    Q = len(IC)
+    # M = M
+    # Q = len(IC)
 
     # XXX: Initialse the solver
     s = Solver()
@@ -64,7 +65,7 @@ def mpc(N, fs, refx, refu, IS, IC, w, la, consxl, consxr, consul, consur,
 
     # Add the constraint for the initial_control and inital_state
     [s.add(xs[i] == j) for i, j in enumerate(IS)]
-    [s.add(us[i] == j) for i, j in enumerate(IC)]
+    # [s.add(us[i] == j) for i, j in enumerate(IC)]
 
     # Add the evolution of the state variables
     k = 0
@@ -85,10 +86,10 @@ def mpc(N, fs, refx, refu, IS, IC, w, la, consxl, consxr, consul, consur,
 
     # Add the linear constraints for control inputs
     consul = consul*N
-    [s.add(us[i+Q] >= consul[i])
+    [s.add(us[i] >= consul[i])
      for i in range(len(consul))]
     consur = consur*N
-    [s.add(us[i+Q] <= consur[i])
+    [s.add(us[i] <= consur[i])
      for i in range(len(consur))]
 
     # Now add the cost function
@@ -97,33 +98,31 @@ def mpc(N, fs, refx, refu, IS, IC, w, la, consxl, consxr, consul, consur,
     refu = [j for i in refu for j in i]
     la = la*N
 
+    def mabs(x):
+        return If(x >= 0, x, -x)
     # XXX: Make the minimisation objective
     obj = Real('objective')
     if norm == 2:
         Dxs = [w[i]*(xs[i+M]-refx[i])**2
                for i in range(len(refx))]
-        Dus = [la[i]*(us[i+Q]-refu[i])**2
+        Dus = [la[i]*(us[i]-refu[i])**2
                for i in range(len(refu))]
         s.add(obj == sum(Dxs) + sum(Dus))
 
     elif norm == 1:
-        def abs(x):
-            return If(x >= 0, x, -x)
 
-        Dxs = [w[i]*abs(xs[i+M]-refx[i])
+        Dxs = [w[i]*mabs(xs[i+M]-refx[i])
                for i in range(len(refx))]
-        Dus = [la[i]*abs(us[i+Q]-refu[i])
+        Dus = [la[i]*mabs(us[i]-refu[i])
                for i in range(len(refu))]
         s.add(obj == sum(Dxs) + sum(Dus))
 
     # XXX: Infinity norm
     elif norm is None:
-        def abs(x):
-            return If(x >= 0, x, -x)
 
-        Dxs = [w[i]*abs(xs[i+M]-refx[i])
+        Dxs = [w[i]*mabs(xs[i+M]-refx[i])
                for i in range(len(refx))]
-        Dus = [la[i]*abs(us[i+Q]-refu[i])
+        Dus = [la[i]*mabs(us[i]-refu[i])
                for i in range(len(refu))]
 
         s.add(obj == mreduce(0, Dxs + Dus))
@@ -139,42 +138,74 @@ def mpc(N, fs, refx, refu, IS, IC, w, la, consxl, consxr, consul, consur,
         l, u = minimize(s, obj, objv)
         # XXX: Do the binary search
         res, model = binary_search(l, u, s, obj)
-        # print('-------------solved model------------------')
-        # print(s)
-        print('---------------answer----------------------')
-        print(res, '\n', model)
+        # XXX: Get the control vector without the zeroth time
+        toret = [(float(model[ret].numerator_as_long())
+                  / float(model[ret].denominator_as_long()))
+                 for ret in us[:Q]]
+        if DEBUG:
+            import sys
+            osout = sys.stdout
+            with open('/tmp/DEBUG.txt', 'a') as f:
+                sys.stdout = f
+                print(s.to_smt2())
+                sys.stdout = osout
+                print('---------------answer----------------------')
+                print(res, '\n', model)
+        return toret
     else:
         print('Model cannot be satisfied')
 
 
-if __name__ == '__main__':
-    # Made up example for model predictive control
-    f1 = (lambda x: x[0]*x[1]+x[2])
-    f2 = (lambda x: 0*x[0]+x[1]**2+0.5*x[2])
-    N = 3
-    fs = [f1, f2]
-    # refx = [[0.8, 0.7], [0, 1], [1, 1]]
-    refx = [[0.8, 0.7]]*N
-    refu = [[0]]*N
-    IS = [5, 0.89]
-    IC = [0]
-    w = [.5, 0.8]
-    la = [0]
-    consxl = [-10, -10]
-    consxr = [10, 10]
-    consul = [-1]
-    consur = [1]
-    # XXX: This is OK, l₁ norm
-    print('------------l₁ norm cost function-------------')
-    mpc(N, fs, refx, refu, IS, IC, w, la, consxl, consxr, consul, consur,
-        norm=1)
-    # XXX: l₂ norm takes too long if the system evolution is linear, but why?
-    print('------------l₂ norm cost function-------------')
-    mpc(N, fs, refx, refu, IS, IC, w, la, consxl, consxr, consul, consur,
-        norm=2)
-    # XXX: This is the l∞ norm
-    print('------------l∞ norm cost function-------------')
-    mpc(N, fs, refx, refu, IS, IC, w, la, consxl, consxr, consul, consur,
-        norm=None)
+def example():
+    # XXX: Model a simple linear moving robot at constant velocity with
+    # disturbance. Control it using MPC
+    # The step-size
+    d = 0.01
+    # The time horizon (second)
+    h = 0.1
+    N = int(h//d)   # The number of steps in MPC
+    p = (lambda x: x[0] + (1 + x[0]*0.1*x[1])*d)     # Plant model (Euler)
 
-    # https://www.alglib.net/translator/man/manual.cpython.html#example_minqp_d_lc1
+    # XXX: The noisy plant model, which we don't know about
+    pn = (lambda x: x[0] + numpy.random.rand() + (1 + x[0]*0.1*x[1])*d)
+    # FIXME: If the std-deviation is huge then SMT seems to crap out
+
+    rx = [[5]]*N    # The ref point for system state
+    ru = [[0]]*N    # The ref point for control input
+    # XXX: The bounds for state and control inputs
+    xl = []
+    xu = []
+    ul = [-5000]
+    uu = [5000]
+    # XXX: Optimisation weights, equal optimisation
+    xw = [1]
+    uw = [0]
+    # XXX: Initial values for state and control inputs
+    x0 = [0.5]
+
+    actions = []
+    xs = [x0]
+    count = 0
+    # XXX: Start simulating the movement of the robot
+    while(count <= h):
+        # XXX: This is OK, l₁ norm
+        print('------------l₁ norm cost function-------------')
+        u0 = MPC(N, 1, 1, [p], rx, ru, x0, xw, uw, xl, xu, ul,
+                 uu, norm=1, DEBUG=False)
+
+        # XXX: Apply the action to the plant
+        # FIXME: Make this plant model have some randomness
+        x0 = [pn(x0 + u0)]
+        print(x0, u0)
+        # Append to list for plotting
+        actions += [u0]
+        xs += [x0]
+        # Increment time
+        count += d
+
+    print('xs:', xs)
+    print('us:', actions)
+
+
+if __name__ == '__main__':
+    example()
