f77ec3c12d5dfdf7ae610598562f55038e810612
Avinash Malik <avinash.malik@auckland.ac.nz>
2020-12-14 21:13:38 +1300

    Added the robot example. Need to make simulink discrete updates.
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

---
+++ mpc/robot.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import src.mpc as SMPC
+import numpy
+from scipy import io
+import matplotlib.pyplot as plt
+import importlib
+# from math import pi, ceil
+from z3 import If, And, Or
+
+
+def set_plt_params():
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.serif'] = 'Ubuntu'
+    plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+    plt.rcParams['font.size'] = 10
+    plt.rcParams['axes.labelsize'] = 10
+    plt.rcParams['axes.labelweight'] = 'bold'
+    plt.rcParams['axes.titlesize'] = 10
+    plt.rcParams['xtick.labelsize'] = 8
+    plt.rcParams['ytick.labelsize'] = 8
+    plt.rcParams['legend.fontsize'] = 10
+    plt.rcParams['figure.titlesize'] = 12
+
+
+def robot():
+    # XXX: Follow the trajectory of the robot from the QSS paper. The
+    # trajectory is generated from simulink.
+    d = 0.05                    # The step-size
+    h = 2                      # Time horizon
+    N = 1   # The number of steps to predict + control
+
+    # The plant model with uncertainty. Argument x is a vector of state
+    # + control vectors
+
+    # XXX: Supporting functions
+    cos = (lambda t: 1 - t**2/2 + t**4/24)
+    sin = (lambda t: t - t**3/6 + t**5/120)
+    tan = (lambda t: t + t**3/6 + 2*t**5/15)
+
+    # cos = (lambda t: 1 - t**2/2)
+    # sin = (lambda t: t - t**3/6)
+    # tan = (lambda t: t + t**3/3)
+    # XXX: The length of the robotic link
+    ll = 1
+
+    # XXX: The guard
+    g = (lambda x, y: Or(And(y >= 1.8, x <= 2.8), And(y <= -0.8, x <= 2.8)))
+    gn = (lambda x, y: (y >= 1.8 and x <= 2.8) or (y <= -0.8 and x <= 2.8))
+
+    # XXX: The evolution of x
+    px = (lambda x: x[0] + If(g(x[0], x[1]), 0, cos(x[2])*x[4])*d)
+    # XXX: The real one with noise
+    pnx = (lambda x: x[0] + numpy.random.rand()*0.00 +
+           (0 if(gn(x[0], x[1])) else cos(x[2])*x[4]*d))
+
+    # FIXME: I might need to fix simulink -- make it discrete
+
+    # XXX: The evolution of y
+    py = (lambda x: x[1] + If(g(x[0], x[1]), 0, sin(x[2])*-x[5])*d)
+    pny = (lambda x: x[1] + numpy.random.rand()*0.00 +
+           (0 if(gn(x[0], x[1])) else sin(x[2])*-x[5]*d))
+    # XXX: The evolution of th
+    pth = (lambda x: x[2] + If(g(x[0], x[1]), 0, tan(x[3])/ll*x[6])*d)
+    pnth = (lambda x: x[2] + numpy.random.rand()*0.00 +
+            (0 if(gn(x[0], x[1])) else tan(x[3])/ll*x[6]*d))
+    # XXX: Evolution of ph
+    pph = (lambda x: x[3] + If(g(x[0], x[1]), 0, -x[7])*d)
+    pnph = (lambda x: x[3] + numpy.random.rand()*0.00 +
+            (0 if(gn(x[0], x[1])) else -x[7]*d))
+    # XXX: All the dynamics together
+    ps = [px, py, pth, pph]
+    pns = [pnx, pny, pnth, pnph]
+
+    # XXX: The reference trajectory from simulink
+    m = io.loadmat('/Users/amal029_old/eha/mpc/robottraj.mat')
+    # XXX: The inital state variables
+    x0 = [m['xout'][0][0], m['yout'][0][0], m['thout'][0][0],
+          m['phout'][0][0]]
+
+    # XXX: Reference for control inputs
+    rus = [[2, 30, 2, 1.1]]*N
+
+    # XXX: References (this should be inside the loop) -- sliding window
+    rxs = [[m['xout'][i][0], m['yout'][i][0],
+            m['thout'][i][0], m['phout'][i][0]]
+           for i in range(1, N+1, 1)]
+
+    # XXX: Bounds for state variables
+    xl = [-2, -2, -2, -2]*N
+    xu = [2, 2, 2, 2]*N
+
+    # XXX: Bounds for control inputs
+    ul = [0, 0, 0, 0]*N
+    uu = [2, 30, 2, 2]*N
+
+    # XXX: Weights for optimisation
+    wx = [0, 0, 0, 0]*N
+    wu = [0.1, 1, 0.1, 0.1]*N
+
+    # XXX: Start simulating
+    actions = []
+    ts = [0]
+    xs = [x0]
+    count = 1
+    # XXX: The solver
+    s = SMPC.MPC(N, 4, 4, ps, xl, xu, ul, uu, norm=1)
+    print(x0)
+    while(True):
+        print('------------l∞ norm cost function-------------')
+        u0 = s.solve(x0, rxs, rus, wx, wu)
+        x0 = [p(x0+u0) for p in pns]
+        print('us: ', u0)
+        print(x0)
+        xs.append(x0)
+        actions.append(u0)
+        ts.append(count*d)
+        # assert(True is False)
+        count += 1
+        if (count == h/d-N+1):
+            break
+        rxs = [[m['xout'][i][0], m['yout'][i][0],
+                m['thout'][i][0], m['phout'][i][0]]
+               for i in range(1, N+1, 1)]
+    return xs, actions, ts, m
+
+
+if __name__ == '__main__':
+    importlib.reload(SMPC)
+    set_plt_params()
+    xs, actions, ts, m = robot()
+    print(ts)
+    xxs = [i[0] for i in xs]
+    yys = [i[1] for i in xs]
+    plt.style.use('ggplot')
+    plt.plot(xxs, yys)
+    plt.plot(m['xout'][:len(m['xout'])-1],
+             m['yout'][:len(m['yout'])-1])
+    plt.show()
--- src/mpc.py
+++ src/mpc.py
@@ -5,12 +5,13 @@ from z3 import Real, Solver, sat, If
 
 class MPC:
 
-    def __init__(self, N, M, Q, fs, refx, refu, w, la, consxl, consxr,
+    def __init__(self, N, M, Q, fs, consxl, consxr,
                  consul, consur, norm=None, DEBUG=False):
         self.N = N
         self.M = M
         self.Q = Q
         self.DEBUG = DEBUG
+        self.norm = norm
 
         # XXX: Initialse the solver
         self.s = Solver()
@@ -35,58 +36,64 @@ class MPC:
             k += Q
 
         # Add the linear constraints for system states
-        consxl = consxl*N if len(consxl) == 1 else consxl
+        consxl = consxl*N if len(consxl) == M else consxl
         [self.s.add(self.xs[i+M] >= consxl[i])
          for i in range(len(consxl)) if i is not None]
-        consxr = consxr*N if len(consxr) == 1 else consxr
+        consxr = consxr*N if len(consxr) == M else consxr
         [self.s.add(self.xs[i+M] <= consxr[i])
          for i in range(len(consxr)) if i is not None]
 
         # Add the linear constraints for control inputs
-        consul = consul*N if len(consul) == 1 else consul
+        consul = consul*N if len(consul) == M else consul
         [self.s.add(self.us[i] >= consul[i])
          for i in range(len(consul)) if i is not None]
-        consur = consur*N if len(consur) == 1 else consur
+        consur = consur*N if len(consur) == M else consur
         [self.s.add(self.us[i] <= consur[i])
          for i in range(len(consur)) if i is not None]
 
-        # Now add the cost function
-        refx = [j for i in refx for j in i]
-        w = w*N
-        refu = [j for i in refu for j in i]
-        la = la*N
-
-        def mabs(x):
-            return If(x >= 0, x, -x)
-
         # XXX: Make the minimisation objective
         self.obj = Real('objective')
-        if norm == 2:
-            Dxs = [w[i]*(self.xs[i+M]-refx[i])**2
+
+    def add_objective(self, refx, refu, w, la):
+        # Now add the cost function
+        refx = [j for i in refx for j in i]  # flatten list
+        w = w*self.N if len(w) == self.M else w
+        refu = [j for i in refu for j in i]
+        la = la*self.N if len(la) == self.Q else la
+
+        if self.norm == 2:
+            Dxs = [w[i]*(self.xs[i+self.M]-refx[i])**2
                    for i in range(len(refx))]
             Dus = [la[i]*(self.us[i]-refu[i])**2
                    for i in range(len(refu))]
             self.s.add(self.obj == sum(Dxs) + sum(Dus))
 
-        elif norm == 1:
-            Dxs = [w[i]*mabs(self.xs[i+M]-refx[i])
+        elif self.norm == 1:
+            Dxs = [w[i]*MPC.mabs(self.xs[i+self.M]-refx[i])
                    for i in range(len(refx))]
-            Dus = [la[i]*mabs(self.us[i]-refu[i])
+            Dus = [la[i]*MPC.mabs(self.us[i]-refu[i])
                    for i in range(len(refu))]
             self.s.add(self.obj == sum(Dxs) + sum(Dus))
 
         # XXX: Infinity norm
-        elif norm is None:
-            Dxs = [w[i]*mabs(self.xs[i+M]-refx[i])
+        elif self.norm is None:
+            Dxs = [w[i]*MPC.mabs(self.xs[i+self.M]-refx[i])
                    for i in range(len(refx))]
-            Dus = [la[i]*mabs(self.us[i]-refu[i])
+            Dus = [la[i]*MPC.mabs(self.us[i]-refu[i])
                    for i in range(len(refu))]
 
-            Dxxs = [MPC.mreduce(0, Dxs[i:i+M]) for i in range(0, len(Dxs), M)]
-            Duus = [MPC.mreduce(0, Dxs[i:i+Q]) for i in range(0, len(Dus), Q)]
+            Dxxs = [MPC.mreduce(0, Dxs[i:i+self.M])
+                    for i in range(0, len(Dxs), self.M)]
+            Duus = [MPC.mreduce(0, Dus[i:i+self.Q])
+                    for i in range(0, len(Dus), self.Q)]
             self.s.add(self.obj == sum(Dxxs + Duus))
             # self.s.add(self.obj ==
             # (MPC.mreduce(0, Dxs) + MPC.mreduce(0, Dus)))
+        pass
+
+    @staticmethod
+    def mabs(x):
+        return If(x >= 0, x, -x)
 
     @staticmethod
     def minimize(s, obj, objv, epsilon=1e-6):
@@ -131,9 +138,11 @@ class MPC:
         else:
             return max(Ej[0], MPC.mreduce(sV, Ej[1:]))
 
-    def solve(self, IS, plan=False):
+    def solve(self, IS, refx, refu, wx, wu, plan=False):
 
         self.s.push()           # put a point to push
+        # XXX: Add the objective constraint
+        self.add_objective(refx, refu, wx, wu)
         # Add the constraint for the initial_control and inital_state
         [self.s.add(self.xs[i] == j) for i, j in enumerate(IS)]
 
